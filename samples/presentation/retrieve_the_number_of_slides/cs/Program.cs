2b284b6c7e9f3c609ca2764a6892f9389c30c3f4
Michael Bowen <mikee.bowen@gmail.com>
2023-10-27 10:35:26 -0700

    fix presentation samples

--- samples/presentation/retrieve_the_number_of_slides/cs/Program.cs
+++ samples/presentation/retrieve_the_number_of_slides/cs/Program.cs
@@ -1,37 +1,49 @@
-#nullable disable
-
 using DocumentFormat.OpenXml.Packaging;
 using System;
 using System.Linq;
 
+if (args.Length >= 2)
+{
+    RetrieveNumberOfSlides(args[0], args[1]);
+}
+else
+{
+    RetrieveNumberOfSlides(args[0]);
+}
+
 static int RetrieveNumberOfSlides(string fileName,
-    bool includeHidden = true)
+    string includeHidden = "true")
 {
     int slidesCount = 0;
 
-    using (PresentationDocument doc =
-        PresentationDocument.Open(fileName, false))
+    using (PresentationDocument doc = PresentationDocument.Open(fileName, false))
     {
-        // Get the presentation part of the document.
-        PresentationPart presentationPart = doc.PresentationPart;
-        if (presentationPart != null)
+        if (doc is not null && doc.PresentationPart is not null)
         {
-            if (includeHidden)
+            // Get the presentation part of the document.
+            PresentationPart presentationPart = doc.PresentationPart;
+            if (presentationPart is not null)
             {
-                slidesCount = presentationPart.SlideParts.Count();
-            }
-            else
-            {
-                // Each slide can include a Show property, which if hidden 
-                // will contain the value "0". The Show property may not 
-                // exist, and most likely will not, for non-hidden slides.
-                var slides = presentationPart.SlideParts.Where(
-                    (s) => (s.Slide != null) &&
-                      ((s.Slide.Show == null) || (s.Slide.Show.HasValue &&
-                      s.Slide.Show.Value)));
-                slidesCount = slides.Count();
+                if (includeHidden.ToLower() == "true")
+                {
+                    slidesCount = presentationPart.SlideParts.Count();
+                }
+                else
+                {
+                    // Each slide can include a Show property, which if hidden 
+                    // will contain the value "0". The Show property may not 
+                    // exist, and most likely will not, for non-hidden slides.
+                    var slides = presentationPart.SlideParts.Where(
+                        (s) => (s.Slide is not null) &&
+                          ((s.Slide.Show is null) || (s.Slide.Show.HasValue && s.Slide.Show.Value)));
+
+                    slidesCount = slides.Count();
+                }
             }
         }
     }
+
+    Console.WriteLine($"Slide Count: {slidesCount}");
+
     return slidesCount;
 }
