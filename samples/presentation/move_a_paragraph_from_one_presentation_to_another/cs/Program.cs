0b199fffba5a547a51e809e6e288ada3823bac21
Michael Bowen <mikee.bowen@gmail.com>
2023-11-10 11:35:56 -0800

    fix additional issues

--- samples/presentation/move_a_paragraph_from_one_presentation_to_another/cs/Program.cs
+++ samples/presentation/move_a_paragraph_from_one_presentation_to_another/cs/Program.cs
@@ -1,66 +1,73 @@
 
-    using System.Linq;
-    using DocumentFormat.OpenXml.Presentation;
-    using DocumentFormat.OpenXml.Packaging;
-    using Drawing = DocumentFormat.OpenXml.Drawing;
+using System.Linq;
+using DocumentFormat.OpenXml.Presentation;
+using DocumentFormat.OpenXml.Packaging;
+using Drawing = DocumentFormat.OpenXml.Drawing;
+using System;
 
 
-    // Moves a paragraph range in a TextBody shape in the source document
-    // to another TextBody shape in the target document.
-    public static void MoveParagraphToPresentation(string sourceFile, string targetFile)
+// Moves a paragraph range in a TextBody shape in the source document
+// to another TextBody shape in the target document.
+static void MoveParagraphToPresentation(string sourceFile, string targetFile)
+{
+    // Open the source file as read/write.
+    using (PresentationDocument sourceDoc = PresentationDocument.Open(sourceFile, true))
     {
-        // Open the source file as read/write.
-        using (PresentationDocument sourceDoc = PresentationDocument.Open(sourceFile, true))
+        // Open the target file as read/write.
+        using (PresentationDocument targetDoc = PresentationDocument.Open(targetFile, true))
         {
-            // Open the target file as read/write.
-            using (PresentationDocument targetDoc = PresentationDocument.Open(targetFile, true))
-            {
-                // Get the first slide in the source presentation.
-                SlidePart slide1 = GetFirstSlide(sourceDoc);
+            // Get the first slide in the source presentation.
+            SlidePart slide1 = GetFirstSlide(sourceDoc);
 
-                // Get the first TextBody shape in it.
-                TextBody textBody1 = slide1.Slide.Descendants<TextBody>().First();
+            // Get the first TextBody shape in it.
+            TextBody textBody1 = slide1.Slide.Descendants<TextBody>().First();
 
-                // Get the first paragraph in the TextBody shape.
-                // Note: "Drawing" is the alias of namespace DocumentFormat.OpenXml.Drawing
-                Drawing.Paragraph p1 = textBody1.Elements<Drawing.Paragraph>().First();
+            // Get the first paragraph in the TextBody shape.
+            // Note: "Drawing" is the alias of namespace DocumentFormat.OpenXml.Drawing
+            Drawing.Paragraph p1 = textBody1.Elements<Drawing.Paragraph>().First();
 
-                // Get the first slide in the target presentation.
-                SlidePart slide2 = GetFirstSlide(targetDoc);
+            // Get the first slide in the target presentation.
+            SlidePart slide2 = GetFirstSlide(targetDoc);
 
-                // Get the first TextBody shape in it.
-                TextBody textBody2 = slide2.Slide.Descendants<TextBody>().First();
+            // Get the first TextBody shape in it.
+            TextBody textBody2 = slide2.Slide.Descendants<TextBody>().First();
 
-                // Clone the source paragraph and insert the cloned. paragraph into the target TextBody shape.
-                // Passing "true" creates a deep clone, which creates a copy of the 
-                // Paragraph object and everything directly or indirectly referenced by that object.
-                textBody2.Append(p1.CloneNode(true));
+            // Clone the source paragraph and insert the cloned. paragraph into the target TextBody shape.
+            // Passing "true" creates a deep clone, which creates a copy of the 
+            // Paragraph object and everything directly or indirectly referenced by that object.
+            textBody2.Append(p1.CloneNode(true));
 
-                // Remove the source paragraph from the source file.
-                textBody1.RemoveChild<Drawing.Paragraph>(p1);
+            // Remove the source paragraph from the source file.
+            textBody1.RemoveChild<Drawing.Paragraph>(p1);
 
-                // Replace the removed paragraph with a placeholder.
-                textBody1.AppendChild<Drawing.Paragraph>(new Drawing.Paragraph());
+            // Replace the removed paragraph with a placeholder.
+            textBody1.AppendChild<Drawing.Paragraph>(new Drawing.Paragraph());
 
-                // Save the slide in the source file.
-                slide1.Slide.Save();
+            // Save the slide in the source file.
+            slide1.Slide.Save();
 
-                // Save the slide in the target file.
-                slide2.Slide.Save();
-            }
+            // Save the slide in the target file.
+            slide2.Slide.Save();
         }
     }
+}
 
-    // Get the slide part of the first slide in the presentation document.
-    public static SlidePart GetFirstSlide(PresentationDocument presentationDocument)
+// Get the slide part of the first slide in the presentation document.
+static SlidePart GetFirstSlide(PresentationDocument presentationDocument)
+{
+    // Get relationship ID of the first slide
+    PresentationPart part = presentationDocument.PresentationPart ?? presentationDocument.AddPresentationPart();
+    SlideIdList slideIdList = part.Presentation.SlideIdList ?? part.Presentation.AppendChild(new SlideIdList());
+    SlideId slideId = part.Presentation.SlideIdList?.GetFirstChild<SlideId>() ?? slideIdList.AppendChild<SlideId>(new SlideId());
+    string? relId = slideId.RelationshipId;
+
+    if (relId is null)
     {
-        // Get relationship ID of the first slide
-        PresentationPart part = presentationDocument.PresentationPart;
-        SlideId slideId = part.Presentation.SlideIdList.GetFirstChild<SlideId>();
-        string relId = slideId.RelationshipId;
-
-        // Get the slide part by the relationship ID.
-        SlidePart slidePart = (SlidePart)part.GetPartById(relId);
-
-        return slidePart;
+        throw new ArgumentNullException(nameof(relId));
     }
+
+    // Get the slide part by the relationship ID.
+    SlidePart slidePart = (SlidePart)part.GetPartById(relId);
+
+    return slidePart;
+}
--- samples/word/add_a_new_part_that_receives_a_relationship_id_to_a_package/cs/Program.cs
+++ samples/word/add_a_new_part_that_receives_a_relationship_id_to_a_package/cs/Program.cs
@@ -1,48 +1,46 @@
 
-    using System.IO;
-    using DocumentFormat.OpenXml;
-    using DocumentFormat.OpenXml.Packaging;
-    using DocumentFormat.OpenXml.Wordprocessing;
-    using System.Xml;
+using System.IO;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Xml;
 
 
-    public static void AddNewPart(string document)
+static void AddNewPart(string document)
+{
+    // Create a new word processing document.
+    WordprocessingDocument wordDoc =
+       WordprocessingDocument.Create(document,
+       WordprocessingDocumentType.Document);
+
+    // Add the MainDocumentPart part in the new word processing document.
+    var mainDocPart = wordDoc.AddNewPart<MainDocumentPart>
+("application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml", "rId1");
+    mainDocPart.Document = new Document();
+
+    // Add the CustomFilePropertiesPart part in the new word processing document.
+    var customFilePropPart = wordDoc.AddCustomFilePropertiesPart();
+    customFilePropPart.Properties = new DocumentFormat.OpenXml.CustomProperties.Properties();
+
+    // Add the CoreFilePropertiesPart part in the new word processing document.
+    var coreFilePropPart = wordDoc.AddCoreFilePropertiesPart();
+    using (XmlTextWriter writer = new
+XmlTextWriter(coreFilePropPart.GetStream(FileMode.Create), System.Text.Encoding.UTF8))
     {
-        // Create a new word processing document.
-        WordprocessingDocument wordDoc =
-           WordprocessingDocument.Create(document,
-           WordprocessingDocumentType.Document);
-
-        // Add the MainDocumentPart part in the new word processing document.
-        var mainDocPart = wordDoc.AddNewPart<MainDocumentPart>
-    ("application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml", "rId1");
-        mainDocPart.Document = new Document();
-
-        // Add the CustomFilePropertiesPart part in the new word processing document.
-        var customFilePropPart = wordDoc.AddCustomFilePropertiesPart();
-        customFilePropPart.Properties = new DocumentFormat.OpenXml.CustomProperties.Properties();
-
-        // Add the CoreFilePropertiesPart part in the new word processing document.
-        var coreFilePropPart = wordDoc.AddCoreFilePropertiesPart();
-        using (XmlTextWriter writer = new 
-    XmlTextWriter(coreFilePropPart.GetStream(FileMode.Create), System.Text.Encoding.UTF8))
-        {
-            writer.WriteRaw("<?xml version=
-    \"1.0\" encoding=\"UTF-8\"?>\r\n<cp:coreProperties xmlns:cp=\
-    "https://schemas.openxmlformats.org/package/2006/metadata/core-properties\"></cp:coreProperties>");
-            writer.Flush();
-        }
-
-        // Add the DigitalSignatureOriginPart part in the new word processing document.
-        wordDoc.AddNewPart<DigitalSignatureOriginPart>("rId4");
-
-        // Add the ExtendedFilePropertiesPart part in the new word processing document.
-        var extendedFilePropPart = wordDoc.AddNewPart<ExtendedFilePropertiesPart>("rId5");
-        extendedFilePropPart.Properties = 
-    new DocumentFormat.OpenXml.ExtendedProperties.Properties();
-
-        // Add the ThumbnailPart part in the new word processing document.
-        wordDoc.AddNewPart<ThumbnailPart>("image/jpeg", "rId6");
-
-        wordDoc.Close();
+        writer.WriteRaw("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<cp:coreProperties xmlns:cp=\"https://schemas.openxmlformats.org/package/2006/metadata/core-properties\"></cp:coreProperties>");
+        writer.Flush();
     }
+
+    // Add the DigitalSignatureOriginPart part in the new word processing document.
+    wordDoc.AddNewPart<DigitalSignatureOriginPart>("rId4");
+
+    // Add the ExtendedFilePropertiesPart part in the new word processing document.
+    var extendedFilePropPart = wordDoc.AddNewPart<ExtendedFilePropertiesPart>("rId5");
+    extendedFilePropPart.Properties =
+new DocumentFormat.OpenXml.ExtendedProperties.Properties();
+
+    // Add the ThumbnailPart part in the new word processing document.
+    wordDoc.AddNewPart<ThumbnailPart>("image/jpeg", "rId6");
+
+    wordDoc.Dispose();
+}
