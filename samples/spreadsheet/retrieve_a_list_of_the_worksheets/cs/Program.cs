bad9934987d65333fd10934530d27c1da84142f3
Michael Bowen <mikee.bowen@gmail.com>
2023-11-16 13:04:45 -0800

    closes #282

--- samples/spreadsheet/retrieve_a_list_of_the_worksheets/cs/Program.cs
+++ samples/spreadsheet/retrieve_a_list_of_the_worksheets/cs/Program.cs
@@ -1,20 +1,38 @@
+// <Snippet0>
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+// <Snippet2>
+// <Snippet1>
+Sheets? sheets = GetAllWorksheets(args[0]);
+// </Snippet1>
 
-GetAllWorksheets(args[0]);
-
+if (sheets is not null)
+{
+    foreach (Sheet sheet in sheets)
+    {
+        Console.WriteLine(sheet.Name);
+    }
+}
+// </Snippet2>
 // Retrieve a List of all the sheets in a workbook.
 // The Sheets class contains a collection of 
 // OpenXmlElement objects, each representing one of 
 // the sheets.
 static Sheets? GetAllWorksheets(string fileName)
 {
+    // <Snippet3>
     Sheets? theSheets = null;
-
+    // </Snippet3>
+    // <Snippet4>
     using (SpreadsheetDocument document = SpreadsheetDocument.Open(fileName, false))
     {
+        // <Snippet5>
         theSheets = document?.WorkbookPart?.Workbook.Sheets;
+        // </Snippet5>
+        // </Snippet4>
     }
 
     return theSheets;
 }
+// </Snippet0>
