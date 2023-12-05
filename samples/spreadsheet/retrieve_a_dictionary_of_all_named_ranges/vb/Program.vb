06dd059488263367168307789fab86b7745ae04f
Michael Bowen <mikee.bowen@gmail.com>
2023-12-05 14:27:58 -0800

    closes #279

--- samples/spreadsheet/retrieve_a_dictionary_of_all_named_ranges/vb/Program.vb
+++ samples/spreadsheet/retrieve_a_dictionary_of_all_named_ranges/vb/Program.vb
@@ -1,26 +1,33 @@
+' <Snippet0>
 Imports DocumentFormat.OpenXml.Packaging
 Imports DocumentFormat.OpenXml.Spreadsheet
 
 Module Program
     Sub Main(args As String())
+        Dim definedNames = GetDefinedNames(args(0))
+
+        For Each definedName In definedNames
+            Console.WriteLine("Name: {0} Value: {1}", definedName.Key, definedName.Value)
+        Next
+
     End Sub
 
-
-
-    Public Function GetDefinedNames(
-        ByVal fileName As String) As Dictionary(Of String, String)
+    Public Function GetDefinedNames(ByVal fileName As String) As Dictionary(Of String, String)
 
         ' Given a workbook name, return a dictionary of defined names.
         ' The pairs include the range name and a string representing the range.
         Dim returnValue As New Dictionary(Of String, String)
 
+        ' <Snippet1>
         ' Open the spreadsheet document for read-only access.
-        Using document As SpreadsheetDocument =
-          SpreadsheetDocument.Open(fileName, False)
+        Using document As SpreadsheetDocument = SpreadsheetDocument.Open(fileName, False)
 
             ' Retrieve a reference to the workbook part.
             Dim wbPart As WorkbookPart = document.WorkbookPart
 
+            ' </Snippet1>
+
+            ' <Snippet2>
             ' Retrieve a reference to the defined names collection.
             Dim definedNames As DefinedNames = wbPart.Workbook.DefinedNames
 
@@ -30,7 +37,11 @@ Module Program
                     returnValue.Add(dn.Name.Value, dn.Text)
                 Next
             End If
+
+            '</Snippet2>
         End Using
         Return returnValue
     End Function
 End Module
+
+' </Snippet0>
