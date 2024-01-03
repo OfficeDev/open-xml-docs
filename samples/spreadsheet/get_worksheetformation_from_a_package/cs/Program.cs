d753ffbff57929bf578a4ff3e93401b2b1359b81
Michael Bowen <mikee.bowen@gmail.com>
2024-01-03 15:04:04 -0800

    closes #271

--- samples/spreadsheet/get_worksheetformation_from_a_package/cs/Program.cs
+++ samples/spreadsheet/get_worksheetformation_from_a_package/cs/Program.cs
@@ -1,29 +1,35 @@
-
+// <Snippet0>
 using DocumentFormat.OpenXml.Packaging;
 using System;
-using A = DocumentFormat.OpenXml.OpenXmlAttribute;
-using E = DocumentFormat.OpenXml.OpenXmlElement;
-using S = DocumentFormat.OpenXml.Spreadsheet.Sheets;
+using OpenXmlAttribute = DocumentFormat.OpenXml.OpenXmlAttribute;
+using OpenXmlElement = DocumentFormat.OpenXml.OpenXmlElement;
+using Sheets = DocumentFormat.OpenXml.Spreadsheet.Sheets;
 
-GetSheetInfo(args[0]);
 
 static void GetSheetInfo(string fileName)
 {
     // Open file as read-only.
     using (SpreadsheetDocument mySpreadsheet = SpreadsheetDocument.Open(fileName, false))
     {
-        S? sheets = mySpreadsheet.WorkbookPart?.Workbook?.Sheets;
+        // <Snippet1>
+        Sheets? sheets = mySpreadsheet.WorkbookPart?.Workbook?.Sheets;
+        // </Snippet1>
 
         if (sheets is not null)
         {
             // For each sheet, display the sheet information.
-            foreach (E sheet in sheets)
+            // <Snippet2>
+            foreach (OpenXmlElement sheet in sheets)
             {
-                foreach (A attr in sheet.GetAttributes())
+                foreach (OpenXmlAttribute attr in sheet.GetAttributes())
                 {
                     Console.WriteLine("{0}: {1}", attr.LocalName, attr.Value);
                 }
             }
+            // </Snippet2>
         }
     }
 }
+// </Snippet0>
+
+GetSheetInfo(args[0]);
