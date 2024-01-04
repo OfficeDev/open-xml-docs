cc25cc51ba704f59c6bad6a0f682764147c123a7
Michael Bowen <mikee.bowen@gmail.com>
2024-01-04 10:29:03 -0800

    closes #268

--- samples/spreadsheet/create_by_providing_a_file_name/vb/Program.vb
+++ samples/spreadsheet/create_by_providing_a_file_name/vb/Program.vb
@@ -1,3 +1,4 @@
+' <Snippet0>
 Imports DocumentFormat.OpenXml
 Imports DocumentFormat.OpenXml.Packaging
 Imports DocumentFormat.OpenXml.Spreadsheet
@@ -6,18 +7,24 @@ Imports DocumentFormat.OpenXml.Spreadsheet
 Module MyModule
 
     Sub Main(args As String())
+        CreateSpreadsheetWorkbook(args(0))
     End Sub
 
     Public Sub CreateSpreadsheetWorkbook(ByVal filepath As String)
+
+        ' <Snippet1>
         ' Create a spreadsheet document by supplying the filepath.
         ' By default, AutoSave = true, Editable = true, and Type = xlsx.
-        Dim spreadsheetDocument As SpreadsheetDocument =
-    SpreadsheetDocument.Create(filepath, SpreadsheetDocumentType.Workbook)
+        Dim spreadsheetDocument As SpreadsheetDocument = SpreadsheetDocument.Create(filepath, SpreadsheetDocumentType.Workbook)
+        ' </Snippet1>
 
+        ' <Snippet2>
         ' Add a WorkbookPart to the document.
         Dim workbookpart As WorkbookPart = spreadsheetDocument.AddWorkbookPart
         workbookpart.Workbook = New Workbook
+        ' </Snippet2>
 
+        ' <Snippet3>
         ' Add a WorksheetPart to the WorkbookPart.
         Dim worksheetPart As WorksheetPart = workbookpart.AddNewPart(Of WorksheetPart)()
         worksheetPart.Worksheet = New Worksheet(New SheetData())
@@ -32,10 +39,13 @@ Module MyModule
         sheet.Name = "mySheet"
 
         sheets.Append(sheet)
+        ' </Snippet3>
 
         workbookpart.Workbook.Save()
 
         ' Dispose the document.
         spreadsheetDocument.Dispose()
     End Sub
+    ' </Snippet0>
+
 End Module
