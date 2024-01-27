c1f62e9a80be0a315c3ad0eb7cd1a3aa3be5eca2
Michael Bowen <mikee.bowen@gmail.com>
2024-01-26 16:45:39 -0800

    closes #237

--- samples/word/retrieve_comments/vb/Program.vb
+++ samples/word/retrieve_comments/vb/Program.vb
@@ -1,26 +1,34 @@
+' <Snippet0>
 Imports DocumentFormat.OpenXml.Packaging
 Imports DocumentFormat.OpenXml.Wordprocessing
 
 Module Program
     Sub Main(args As String())
+        GetCommentsFromDocument(args(0))
     End Sub
 
-
-
     Public Sub GetCommentsFromDocument(ByVal fileName As String)
-        Using wordDoc As WordprocessingDocument =
-            WordprocessingDocument.Open(fileName, False)
 
-            Dim commentsPart As WordprocessingCommentsPart =
-                wordDoc.MainDocumentPart.WordprocessingCommentsPart
+        ' <Snippet1>
+        Using wordDoc As WordprocessingDocument = WordprocessingDocument.Open(fileName, False)
 
-            If commentsPart IsNot Nothing AndAlso
-                commentsPart.Comments IsNot Nothing Then
+            If wordDoc.MainDocumentPart Is Nothing Or wordDoc.MainDocumentPart.WordprocessingCommentsPart Is Nothing Then
+                Throw New ArgumentNullException("MainDocumentPart and/or WordprocessingCommentsPart is null.")
+            End If
+            ' </Snippet1>
+
+            ' <Snippet2>
+            Dim commentsPart As WordprocessingCommentsPart = wordDoc.MainDocumentPart.WordprocessingCommentsPart
+
+            If commentsPart IsNot Nothing AndAlso commentsPart.Comments IsNot Nothing Then
                 For Each comment As Comment In
                     commentsPart.Comments.Elements(Of Comment)()
                     Console.WriteLine(comment.InnerText)
                 Next
             End If
+            ' </Snippet2>
+
         End Using
     End Sub
 End Module
+' </Snippet0>
