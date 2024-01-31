ff942b8654505696606053b1595c0cfd90c08828
Michael Bowen <mikee.bowen@gmail.com>
2024-01-31 10:15:14 -0800

    closes #236

--- samples/word/retrieve_application_property_values/cs/Program.cs
+++ samples/word/retrieve_application_property_values/cs/Program.cs
@@ -1,19 +1,23 @@
+// <Snippet0>
 using DocumentFormat.OpenXml.Packaging;
 using System;
-
-GetApplicationProperty(args[0]);
-
 static void GetApplicationProperty(string fileName)
 {
+    // <Snippet1>
     using (WordprocessingDocument document = WordprocessingDocument.Open(fileName, false))
     {
+        // </Snippet1>
+
+        // <Snippet2>
         if (document.ExtendedFilePropertiesPart is null)
         {
             throw new ArgumentNullException("ExtendedFilePropertiesPart is null.");
         }
 
         var props = document.ExtendedFilePropertiesPart.Properties;
+        // </Snippet2>
 
+        // <Snippet3>
         if (props.Company is not null)
             Console.WriteLine("Company = " + props.Company.Text);
 
@@ -22,5 +26,9 @@ static void GetApplicationProperty(string fileName)
 
         if (props.Manager is not null)
             Console.WriteLine("Manager = " + props.Manager.Text);
+        // </Snippet3>
     }
 }
+// </Snippet0>
+
+GetApplicationProperty(args[0]);
