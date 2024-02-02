4f469f865a211b3c3dd8c5c1a406902abfcb3e0f
Michael Bowen <mikee.bowen@gmail.com>
2024-02-02 11:53:28 -0800

    closes #233

--- samples/word/remove_the_headers_and_footers/cs/Program.cs
+++ samples/word/remove_the_headers_and_footers/cs/Program.cs
@@ -1,17 +1,22 @@
+// <Snippet0>
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using System;
 using System.Linq;
 
-RemoveHeadersAndFooters(args[0]);
 
 // Remove all of the headers and footers from a document.
+// <Snippet1>
 static void RemoveHeadersAndFooters(string filename)
+// </Snippet1>
 {
+    // <Snippet3>
     // Given a document name, remove all of the headers and footers
     // from the document.
     using (WordprocessingDocument doc = WordprocessingDocument.Open(filename, true))
+    // </Snippet3>
     {
+        // <Snippet4>
         if (doc.MainDocumentPart is null)
         {
             throw new ArgumentNullException("MainDocumentPart is null.");
@@ -22,13 +27,17 @@ static void RemoveHeadersAndFooters(string filename)
 
         // Count the header and footer parts and continue if there 
         // are any.
-        if (docPart.HeaderParts.Count() > 0 ||
-            docPart.FooterParts.Count() > 0)
+        if (docPart.HeaderParts.Count() > 0 || docPart.FooterParts.Count() > 0)
         {
+            // </Snippet4>
+
+            // <Snippet5>
             // Remove the header and footer parts.
             docPart.DeleteParts(docPart.HeaderParts);
             docPart.DeleteParts(docPart.FooterParts);
+            // </Snippet5>
 
+            // <Snippet6>
             // Get a reference to the root element of the main
             // document part.
             Document document = docPart.Document;
@@ -38,8 +47,8 @@ static void RemoveHeadersAndFooters(string filename)
             // First, create a list of all descendants of type
             // HeaderReference. Then, navigate the list and call
             // Remove on each item to delete the reference.
-            var headers =
-              document.Descendants<HeaderReference>().ToList();
+            var headers = document.Descendants<HeaderReference>().ToList();
+
             foreach (var header in headers)
             {
                 header.Remove();
@@ -48,8 +57,8 @@ static void RemoveHeadersAndFooters(string filename)
             // First, create a list of all descendants of type
             // FooterReference. Then, navigate the list and call
             // Remove on each item to delete the reference.
-            var footers =
-              document.Descendants<FooterReference>().ToList();
+            var footers = document.Descendants<FooterReference>().ToList();
+
             foreach (var footer in footers)
             {
                 footer.Remove();
@@ -57,6 +66,15 @@ static void RemoveHeadersAndFooters(string filename)
 
             // Save the changes.
             document.Save();
+            // </Snippet6>
         }
     }
 }
+// </Snippet0>
+
+// <Snippet2>
+string filename = args[0];
+
+RemoveHeadersAndFooters(filename);
+// </Snippet2>
+
--- samples/word/remove_the_headers_and_footers/vb/Program.vb
+++ samples/word/remove_the_headers_and_footers/vb/Program.vb
@@ -1,31 +1,49 @@
+' <Snippet0>
 Imports DocumentFormat.OpenXml.Packaging
 Imports DocumentFormat.OpenXml.Spreadsheet
 Imports DocumentFormat.OpenXml.Wordprocessing
 
 Module Program
     Sub Main(args As String())
+        ' <Snippet2>
+        Dim filename As String = args(0)
+
+        RemoveHeadersAndFooters(filename)
+        ' </Snippet2>
     End Sub
 
 
     ' To remove all of the headers and footers in a document.
+    ' <Snippet1>
     Public Sub RemoveHeadersAndFooters(ByVal filename As String)
+        ' </Snippet1>
 
+        ' <Snippet3>
         ' Given a document name, remove all of the headers and footers
         ' from the document.
         Using doc = WordprocessingDocument.Open(filename, True)
+            ' </Snippet3>
+
+            ' <Snippet4>
+            If doc.MainDocumentPart Is Nothing Then
+                Throw New ArgumentNullException("MainDocumentPart is Nothing")
+            End If
 
             ' Get a reference to the main document part.
             Dim docPart = doc.MainDocumentPart
 
             ' Count the header and footer parts and continue if there 
             ' are any.
-            If (docPart.HeaderParts.Count > 0) Or
-              (docPart.FooterParts.Count > 0) Then
+            If (docPart.HeaderParts.Count > 0) Or (docPart.FooterParts.Count > 0) Then
+                ' </Snippet4>
 
+                ' <Snippet5>
                 ' Remove the header and footer parts.
                 docPart.DeleteParts(docPart.HeaderParts)
                 docPart.DeleteParts(docPart.FooterParts)
+                ' </Snippet5>
 
+                ' <Snippet6>
                 ' Get a reference to the root element of the main 
                 ' document part.
                 Dim document As Document = docPart.Document
@@ -35,8 +53,8 @@ Module Program
                 ' First, create a list of all descendants of type
                 ' HeaderReference. Then, navigate the list and call
                 ' Remove on each item to delete the reference.
-                Dim headers =
-                  document.Descendants(Of HeaderReference).ToList()
+                Dim headers = document.Descendants(Of HeaderReference).ToList()
+
                 For Each header In headers
                     header.Remove()
                 Next
@@ -44,8 +62,8 @@ Module Program
                 ' First, create a list of all descendants of type
                 ' FooterReference. Then, navigate the list and call
                 ' Remove on each item to delete the reference.
-                Dim footers =
-                  document.Descendants(Of FooterReference).ToList()
+                Dim footers = document.Descendants(Of FooterReference).ToList()
+
                 For Each footer In footers
                     footer.Remove()
                 Next
@@ -53,6 +71,8 @@ Module Program
                 ' Save the changes.
                 document.Save()
             End If
+            ' </Snippet6>
         End Using
     End Sub
 End Module
+' </Snippet0>
