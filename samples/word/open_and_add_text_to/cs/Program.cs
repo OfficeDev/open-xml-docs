455e7761f002316b32295a4756a0c3e005d1f240
Michael Bowen <mikee.bowen@gmail.com>
2024-02-05 11:57:56 -0800

    closes #231

--- samples/word/open_and_add_text_to/cs/Program.cs
+++ samples/word/open_and_add_text_to/cs/Program.cs
@@ -1,12 +1,11 @@
-
+// <Snippet0>
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using System;
 
-OpenAndAddTextToWordDocument(args[0], args[1]);
-
 static void OpenAndAddTextToWordDocument(string filepath, string txt)
 {
+    // <Snippet1>
     // Open a WordprocessingDocument for editing using the filepath.
     WordprocessingDocument wordprocessingDocument = WordprocessingDocument.Open(filepath, true);
 
@@ -14,18 +13,31 @@ static void OpenAndAddTextToWordDocument(string filepath, string txt)
     {
         throw new ArgumentNullException(nameof(wordprocessingDocument));
     }
+    // </Snippet1>
 
+    // <Snippet2>
     // Assign a reference to the existing document body.
     MainDocumentPart mainDocumentPart = wordprocessingDocument.MainDocumentPart ?? wordprocessingDocument.AddMainDocumentPart();
     mainDocumentPart.Document ??= new Document();
     mainDocumentPart.Document.Body ??= mainDocumentPart.Document.AppendChild(new Body());
     Body body = wordprocessingDocument.MainDocumentPart!.Document!.Body!;
+    // </Snippet2>
 
+    // <Snippet3>
     // Add new text.
     Paragraph para = body.AppendChild(new Paragraph());
     Run run = para.AppendChild(new Run());
     run.AppendChild(new Text(txt));
+    // </Snippet3>
 
-    // Dispose the handle explicitly.
+    // There is no using block, so Dispose the handle explicitly.
     wordprocessingDocument.Dispose();
 }
+// </Snippet0>
+
+// <Snippet4>
+string file = args[0];
+string txt = args[1];
+
+OpenAndAddTextToWordDocument(args[0], args[1]);
+// </Snippet4>
--- samples/word/open_and_add_text_to/vb/Program.vb
+++ samples/word/open_and_add_text_to/vb/Program.vb
@@ -1,3 +1,4 @@
+' <Snippet0>
 Imports DocumentFormat.OpenXml.Packaging
 Imports DocumentFormat.OpenXml.Wordprocessing
 
@@ -5,22 +6,49 @@ Imports DocumentFormat.OpenXml.Wordprocessing
 Module MyModule
 
     Sub Main(args As String())
+        ' <Snippet4>
+        Dim file As String = args(0)
+        Dim txt As String = args(1)
+
+        OpenAndAddTextToWordDocument(file, txt)
+        ' </Snippet4>
     End Sub
 
     Public Sub OpenAndAddTextToWordDocument(ByVal filepath As String, ByVal txt As String)
+
+        ' <Snippet1>
         ' Open a WordprocessingDocument for editing using the filepath.
-        Dim wordprocessingDocument As WordprocessingDocument =
-            WordprocessingDocument.Open(filepath, True)
+        Dim wordprocessingDocument As WordprocessingDocument = WordprocessingDocument.Open(filepath, True)
 
+        If wordprocessingDocument Is Nothing Then
+            Throw New ArgumentNullException(NameOf(wordprocessingDocument))
+        End If
+        ' </Snippet1>
+
+        ' <Snippet2>
         ' Assign a reference to the existing document body. 
-        Dim body As Body = wordprocessingDocument.MainDocumentPart.Document.Body
+        Dim mainDocumentPart As MainDocumentPart = If(wordprocessingDocument.MainDocumentPart, wordprocessingDocument.AddMainDocumentPart())
 
+        If wordprocessingDocument.MainDocumentPart.Document Is Nothing Then
+            wordprocessingDocument.MainDocumentPart.Document = New Document()
+        End If
+
+        If wordprocessingDocument.MainDocumentPart.Document.Body Is Nothing Then
+            wordprocessingDocument.MainDocumentPart.Document.Body = New Body()
+        End If
+
+        Dim body As Body = wordprocessingDocument.MainDocumentPart.Document.Body
+        ' </Snippet2>
+
+        ' <Snippet3>
         ' Add new text.
         Dim para As Paragraph = body.AppendChild(New Paragraph)
         Dim run As Run = para.AppendChild(New Run)
         run.AppendChild(New Text(txt))
+        ' </Snippet3>
 
-        ' Dispose the handle explicitly.
+        ' There is not using, so Dispose the handle explicitly.
         wordprocessingDocument.Dispose()
     End Sub
 End Module
+' </Snippet0>
