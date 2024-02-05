00a87e65ee6dd97bd8a2e64578c321b7c3692d89
Michael Bowen <mikee.bowen@gmail.com>
2024-02-05 13:51:10 -0800

    closes #230

--- samples/word/open_from_a_stream/cs/Program.cs
+++ samples/word/open_from_a_stream/cs/Program.cs
@@ -1,4 +1,4 @@
-
+// <Snippet0>
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using System.IO;
@@ -6,21 +6,38 @@ using System.IO;
 
 static void OpenAndAddToWordprocessingStream(Stream stream, string txt)
 {
+    // <Snippet1>
     // Open a WordProcessingDocument based on a stream.
     WordprocessingDocument wordprocessingDocument = WordprocessingDocument.Open(stream, true);
+    // </Snippet1>
 
-    // Assign a reference to the existing document body.
+    // <Snippet2>
+    // Assign a reference to the document body.
     MainDocumentPart mainDocumentPart = wordprocessingDocument.MainDocumentPart ?? wordprocessingDocument.AddMainDocumentPart();
     mainDocumentPart.Document ??= new Document();
     Body body = mainDocumentPart.Document.Body ?? mainDocumentPart.Document.AppendChild(new Body());
+    // </Snippet2>
 
+    // <Snippet3>
     // Add new text.
     Paragraph para = body.AppendChild(new Paragraph());
     Run run = para.AppendChild(new Run());
     run.AppendChild(new Text(txt));
+    // </Snippet3>
 
     // Dispose the document handle.
     wordprocessingDocument.Dispose();
 
     // Caller must close the stream.
 }
+// </Snippet0>
+
+// <Snippet4>
+string filePath = args[0];
+string txt = args[1];
+
+using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+{
+    OpenAndAddToWordprocessingStream(fileStream, txt);
+}
+// </Snippet4>
--- samples/word/open_from_a_stream/vb/Program.vb
+++ samples/word/open_from_a_stream/vb/Program.vb
@@ -1,3 +1,4 @@
+' <Snippet0>
 Imports System.IO
 Imports DocumentFormat.OpenXml.Packaging
 Imports DocumentFormat.OpenXml.Wordprocessing
@@ -6,19 +7,44 @@ Imports DocumentFormat.OpenXml.Wordprocessing
 Module MyModule
 
     Sub Main(args As String())
+        ' <Snippet4>
+        Dim filePath As String = args(0)
+        Dim txt As String = args(1)
+
+        Using fileStream As FileStream = New FileStream(filePath, FileMode.Open)
+            OpenAndAddToWordprocessingStream(fileStream, txt)
+        End Using
+        ' </Snippet4>
     End Sub
 
     Public Sub OpenAndAddToWordprocessingStream(ByVal stream As Stream, ByVal txt As String)
+
+        ' <Snippet1>
         ' Open a WordProcessingDocument based on a stream.
         Dim wordprocessingDocument As WordprocessingDocument = WordprocessingDocument.Open(stream, True)
+        ' </Snippet1>
 
-        ' Assign a reference to the existing document body.
+        ' <Snippet2>
+        ' Assign a reference to the document body. 
+        Dim mainDocumentPart As MainDocumentPart = If(wordprocessingDocument.MainDocumentPart, wordprocessingDocument.AddMainDocumentPart())
+
+        If wordprocessingDocument.MainDocumentPart.Document Is Nothing Then
+            wordprocessingDocument.MainDocumentPart.Document = New Document()
+        End If
+
+        If wordprocessingDocument.MainDocumentPart.Document.Body Is Nothing Then
+            wordprocessingDocument.MainDocumentPart.Document.Body = New Body()
+        End If
+
         Dim body As Body = wordprocessingDocument.MainDocumentPart.Document.Body
+        ' </Snippet2>
 
+        ' <Snippet3>
         ' Add new text.
         Dim para As Paragraph = body.AppendChild(New Paragraph)
         Dim run As Run = para.AppendChild(New Run)
         run.AppendChild(New Text(txt))
+        ' </Snippet3>
 
         ' Dispose the document handle.
         wordprocessingDocument.Dispose()
@@ -26,3 +52,4 @@ Module MyModule
         ' Caller must close the stream.
     End Sub
 End Module
+' </Snippet0>
