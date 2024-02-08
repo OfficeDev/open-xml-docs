37c5abd1ec0fe09c39ed25117565a9548d820b03
Michael Bowen <mikee.bowen@gmail.com>
2024-02-08 12:13:07 -0800

    closes #217

--- samples/word/insert_a_picture/vb/Program.vb
+++ samples/word/insert_a_picture/vb/Program.vb
@@ -1,3 +1,4 @@
+' <Snippet>
 Imports System.IO
 Imports DocumentFormat.OpenXml
 Imports DocumentFormat.OpenXml.Packaging
@@ -6,14 +7,24 @@ Imports A = DocumentFormat.OpenXml.Drawing
 Imports DW = DocumentFormat.OpenXml.Drawing.Wordprocessing
 Imports PIC = DocumentFormat.OpenXml.Drawing.Pictures
 
-Module Program `
-  Sub Main(args As String())`
-  End Sub`
+Module Program
+    Sub Main(args As String())
+        ' <Snippet4>
+        Dim documentPath As String = args(0)
+        Dim picturePath As String = args(1)
 
-  
-                            
+        InsertAPicture(documentPath, picturePath)
+        ' </Snippet4>
+    End Sub
+
+
+
     Public Sub InsertAPicture(ByVal document As String, ByVal fileName As String)
+        ' <Snippet1>
         Using wordprocessingDocument As WordprocessingDocument = WordprocessingDocument.Open(document, True)
+            ' </Snippet1>
+
+            ' <Snippet2>
             Dim mainPart As MainDocumentPart = wordprocessingDocument.MainDocumentPart
 
             Dim imagePart As ImagePart = mainPart.AddImagePart(ImagePartType.Jpeg)
@@ -23,52 +34,56 @@ Module Program `
             End Using
 
             AddImageToBody(wordprocessingDocument, mainPart.GetIdOfPart(imagePart))
+            ' </Snippet2>
         End Using
     End Sub
 
     Private Sub AddImageToBody(ByVal wordDoc As WordprocessingDocument, ByVal relationshipId As String)
+        ' <Snippet3>
         ' Define the reference of the image.
-        Dim element = New Drawing( _
-                              New DW.Inline( _
-                          New DW.Extent() With {.Cx = 990000L, .Cy = 792000L}, _
-                          New DW.EffectExtent() With {.LeftEdge = 0L, .TopEdge = 0L, .RightEdge = 0L, .BottomEdge = 0L}, _
-                          New DW.DocProperties() With {.Id = CType(1UI, UInt32Value), .Name = "Picture1"}, _
-                          New DW.NonVisualGraphicFrameDrawingProperties( _
-                              New A.GraphicFrameLocks() With {.NoChangeAspect = True} _
-                              ), _
-                          New A.Graphic(New A.GraphicData( _
-                                        New PIC.Picture( _
-                                            New PIC.NonVisualPictureProperties( _
-                                                New PIC.NonVisualDrawingProperties() With {.Id = 0UI, .Name = "Koala.jpg"}, _
-                                                New PIC.NonVisualPictureDrawingProperties() _
-                                                ), _
-                                            New PIC.BlipFill( _
-                                                New A.Blip( _
-                                                    New A.BlipExtensionList( _
-                                                        New A.BlipExtension() With {.Uri = "{28A0092B-C50C-407E-A947-70E740481C1C}"}) _
-                                                    ) With {.Embed = relationshipId, .CompressionState = A.BlipCompressionValues.Print}, _
-                                                New A.Stretch( _
-                                                    New A.FillRectangle() _
-                                                    ) _
-                                                ), _
-                                            New PIC.ShapeProperties( _
-                                                New A.Transform2D( _
-                                                    New A.Offset() With {.X = 0L, .Y = 0L}, _
-                                                    New A.Extents() With {.Cx = 990000L, .Cy = 792000L}), _
-                                                New A.PresetGeometry( _
-                                                    New A.AdjustValueList() _
-                                                    ) With {.Preset = A.ShapeTypeValues.Rectangle} _
-                                                ) _
-                                            ) _
-                                        ) With {.Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture"} _
-                                    ) _
-                                ) With {.DistanceFromTop = 0UI, _
-                                        .DistanceFromBottom = 0UI, _
-                                        .DistanceFromLeft = 0UI, _
-                                        .DistanceFromRight = 0UI} _
+        Dim element = New Drawing(
+                              New DW.Inline(
+                          New DW.Extent() With {.Cx = 990000L, .Cy = 792000L},
+                          New DW.EffectExtent() With {.LeftEdge = 0L, .TopEdge = 0L, .RightEdge = 0L, .BottomEdge = 0L},
+                          New DW.DocProperties() With {.Id = CType(1UI, UInt32Value), .Name = "Picture1"},
+                          New DW.NonVisualGraphicFrameDrawingProperties(
+                              New A.GraphicFrameLocks() With {.NoChangeAspect = True}
+                              ),
+                          New A.Graphic(New A.GraphicData(
+                                        New PIC.Picture(
+                                            New PIC.NonVisualPictureProperties(
+                                                New PIC.NonVisualDrawingProperties() With {.Id = 0UI, .Name = "Koala.jpg"},
+                                                New PIC.NonVisualPictureDrawingProperties()
+                                                ),
+                                            New PIC.BlipFill(
+                                                New A.Blip(
+                                                    New A.BlipExtensionList(
+                                                        New A.BlipExtension() With {.Uri = "{28A0092B-C50C-407E-A947-70E740481C1C}"})
+                                                    ) With {.Embed = relationshipId, .CompressionState = A.BlipCompressionValues.Print},
+                                                New A.Stretch(
+                                                    New A.FillRectangle()
+                                                    )
+                                                ),
+                                            New PIC.ShapeProperties(
+                                                New A.Transform2D(
+                                                    New A.Offset() With {.X = 0L, .Y = 0L},
+                                                    New A.Extents() With {.Cx = 990000L, .Cy = 792000L}),
+                                                New A.PresetGeometry(
+                                                    New A.AdjustValueList()
+                                                    ) With {.Preset = A.ShapeTypeValues.Rectangle}
+                                                )
+                                            )
+                                        ) With {.Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture"}
+                                    )
+                                ) With {.DistanceFromTop = 0UI,
+                                        .DistanceFromBottom = 0UI,
+                                        .DistanceFromLeft = 0UI,
+                                        .DistanceFromRight = 0UI}
                             )
 
         ' Append the reference to body, the element should be in a Run.
         wordDoc.MainDocumentPart.Document.Body.AppendChild(New Paragraph(New Run(element)))
+        ' </Snippet3>
     End Sub
 End Module
+' </Snippet>
