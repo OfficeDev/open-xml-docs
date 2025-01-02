000770daf0f7ddc7cb9153c6be92a257abe10a31
Michael Bowen <10384982+mikeebowen@users.noreply.github.com>
2025-01-02 13:08:44 -0800

    closes #182

--- samples/presentation/change_the_fill_color_of_a_shape/vb/Program.vb
+++ samples/presentation/change_the_fill_color_of_a_shape/vb/Program.vb
@@ -1,48 +1,62 @@
-Imports DocumentFormat.OpenXml
 Imports DocumentFormat.OpenXml.Packaging
 Imports DocumentFormat.OpenXml.Presentation
 Imports Drawing = DocumentFormat.OpenXml.Drawing
 
 Module Program
     Sub Main(args As String())
+        SetPPTShapeColor(args(0))
     End Sub
 
-
-
+    ' <Snippet0>
     ' Change the fill color of a shape.
-    ' The test file must have a filled shape as the first shape on the first slide.
-    Public Sub SetPPTShapeColor(ByVal docName As String)
+    ' The test file must have a shape on the first slide.
+    Sub SetPPTShapeColor(docName As String)
+        ' <Snippet1>
         Using ppt As PresentationDocument = PresentationDocument.Open(docName, True)
+            ' </Snippet1>
+            ' <Snippet2>
             ' Get the relationship ID of the first slide.
-            Dim part As PresentationPart = ppt.PresentationPart
-            Dim slideIds As OpenXmlElementList = part.Presentation.SlideIdList.ChildElements
-            Dim relId As String = TryCast(slideIds(0), SlideId).RelationshipId
+            Dim presentationPart As PresentationPart = If(ppt.PresentationPart, ppt.AddPresentationPart())
+            presentationPart.Presentation.SlideIdList = If(presentationPart.Presentation.SlideIdList, New SlideIdList())
+            Dim slideId As SlideId = presentationPart.Presentation.SlideIdList.GetFirstChild(Of SlideId)()
 
-            ' Get the slide part from the relationship ID.
-            Dim slide As SlidePart = DirectCast(part.GetPartById(relId), SlidePart)
+            If slideId IsNot Nothing Then
+                Dim relId As String = slideId.RelationshipId
 
-            If slide IsNot Nothing Then
-                ' Get the shape tree that contains the shape to change.
-                Dim tree As ShapeTree = slide.Slide.CommonSlideData.ShapeTree
+                If relId IsNot Nothing Then
+                    ' Get the slide part from the relationship ID.
+                    Dim slidePart As SlidePart = CType(presentationPart.GetPartById(relId), SlidePart)
+                    ' </Snippet2>
+                    ' <Snippet3>
+                    ' Get or add the shape tree
+                    slidePart.Slide.CommonSlideData = If(slidePart.Slide.CommonSlideData, New CommonSlideData())
 
-                ' Get the first shape in the shape tree.
-                Dim shape As Shape = tree.GetFirstChild(Of Shape)()
+                    ' Get the shape tree that contains the shape to change.
+                    slidePart.Slide.CommonSlideData.ShapeTree = If(slidePart.Slide.CommonSlideData.ShapeTree, New ShapeTree())
 
-                If shape IsNot Nothing Then
-                    ' Get the style of the shape.
-                    Dim style As ShapeStyle = shape.ShapeStyle
+                    ' Get the first shape in the shape tree.
+                    Dim shape As Shape = slidePart.Slide.CommonSlideData.ShapeTree.GetFirstChild(Of Shape)()
 
-                    ' Get the fill reference.
-                    Dim fillRef As Drawing.FillReference = style.FillReference
+                    If shape IsNot Nothing Then
+                        ' Get or add the shape properties element of the shape.
+                        shape.ShapeProperties = If(shape.ShapeProperties, New ShapeProperties())
 
-                    ' Set the fill color to SchemeColor Accent 6;
-                    fillRef.SchemeColor = New Drawing.SchemeColor()
-                    fillRef.SchemeColor.Val = Drawing.SchemeColorValues.Accent6
+                        ' Get or add the fill reference.
+                        Dim solidFill As Drawing.SolidFill = shape.ShapeProperties.GetFirstChild(Of Drawing.SolidFill)()
 
-                    ' Save the modified slide.
-                    slide.Slide.Save()
+                        ' Add solid fill element if it is missing and assign it to solidFill
+                        If solidFill Is Nothing Then
+                            shape.ShapeProperties.AddChild(New Drawing.SolidFill())
+                            solidFill = shape.ShapeProperties.GetFirstChild(Of Drawing.SolidFill)()
+                        End If
+
+                        ' Set the fill color to SchemeColor
+                        solidFill.SchemeColor = New Drawing.SchemeColor() With {.Val = Drawing.SchemeColorValues.Accent2}
+                    End If
+                    ' </Snippet3>
                 End If
             End If
         End Using
     End Sub
+    ' </Snippet0>
 End Module
