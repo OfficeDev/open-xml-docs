b55eaec409c9c59392cef1cf0dd685194396c0a5
Michael Bowen <10384982+mikeebowen@users.noreply.github.com>
2025-01-08 15:10:06 -0800

    closes #320

--- samples/word/add_a_new_part_to_a_package/cs/Program.cs
+++ samples/word/add_a_new_part_to_a_package/cs/Program.cs
@@ -2,13 +2,22 @@
 using DocumentFormat.OpenXml.Packaging;
 using System.IO;
 
+// <Snippet3>
+string document = args[0];
+string fileName = args[1];
+
 AddNewPart(args[0], args[1]);
+// </Snippet3>
 
 // To add a new document part to a package.
+// <Snippet0>
 static void AddNewPart(string document, string fileName)
 {
+    // <Snippet1>
     using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(document, true))
+        // </Snippet1>
     {
+        // <Snippet2>
         MainDocumentPart mainPart = wordDoc.MainDocumentPart ?? wordDoc.AddMainDocumentPart();
 
         CustomXmlPart myXmlPart = mainPart.AddCustomXmlPart(CustomXmlPartType.CustomXml);
@@ -17,5 +26,7 @@ static void AddNewPart(string document, string fileName)
         {
             myXmlPart.FeedData(stream);
         }
+        // </Snippet2>
     }
 }
+// </Snippet0>
