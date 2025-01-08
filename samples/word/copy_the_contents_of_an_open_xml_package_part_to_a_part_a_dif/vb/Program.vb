f6392cbd4e2a27159c467c6be538a20e2b217685
Michael Bowen <10384982+mikeebowen@users.noreply.github.com>
2025-01-08 14:20:17 -0800

    closes #321

--- samples/word/copy_the_contents_of_an_open_xml_package_part_to_a_part_a_dif/vb/Program.vb
+++ samples/word/copy_the_contents_of_an_open_xml_package_part_to_a_part_a_dif/vb/Program.vb
@@ -1,24 +1,41 @@
-Imports System.IO
 Imports DocumentFormat.OpenXml.Packaging
+Imports System.IO
 
-
-Module MyModule
-
+Module Program
     Sub Main(args As String())
+        ' <Snippet4>
+        Dim fromDocument1 As String = args(0)
+        Dim toDocument2 As String = args(1)
+
+        CopyThemeContent(fromDocument1, toDocument2)
+        ' </Snippet4>
     End Sub
 
     ' To copy contents of one package part.
-    Public Sub CopyThemeContent(ByVal fromDocument1 As String, ByVal toDocument2 As String)
-        Dim wordDoc1 As WordprocessingDocument = WordprocessingDocument.Open(fromDocument1, False)
-        Dim wordDoc2 As WordprocessingDocument = WordprocessingDocument.Open(toDocument2, True)
-        Using (wordDoc2)
-            Dim themePart1 As ThemePart = wordDoc1.MainDocumentPart.ThemePart
-            Dim themePart2 As ThemePart = wordDoc2.MainDocumentPart.ThemePart
-            Dim streamReader As StreamReader = New StreamReader(themePart1.GetStream())
-            Dim streamWriter As StreamWriter = New StreamWriter(themePart2.GetStream(FileMode.Create))
-            Using (streamWriter)
-                streamWriter.Write(streamReader.ReadToEnd)
+    ' <Snippet0>
+    ' <Snippet2>
+    Sub CopyThemeContent(fromDocument1 As String, toDocument2 As String)
+        ' <Snippet1>
+        Using wordDoc1 As WordprocessingDocument = WordprocessingDocument.Open(fromDocument1, False)
+            Using wordDoc2 As WordprocessingDocument = WordprocessingDocument.Open(toDocument2, True)
+                ' </Snippet1>
+                Dim themePart1 As ThemePart = wordDoc1?.MainDocumentPart?.ThemePart
+                Dim themePart2 As ThemePart = wordDoc2?.MainDocumentPart?.ThemePart
+                ' </Snippet2>
+
+                ' If the theme parts are null, then there is nothing to copy.
+                If themePart1 Is Nothing OrElse themePart2 Is Nothing Then
+                    Return
+                End If
+                ' <Snippet3>
+                Using streamReader As New StreamReader(themePart1.GetStream())
+                    Using streamWriter As New StreamWriter(themePart2.GetStream(FileMode.Create))
+                        streamWriter.Write(streamReader.ReadToEnd())
+                    End Using
+                End Using
+                ' </Snippet3>
             End Using
         End Using
     End Sub
+    ' </Snippet0>
 End Module
