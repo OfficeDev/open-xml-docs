28f3fd90a5ff92296d8e0f1fdf7470ecc96fb7b4
Michael Bowen <10384982+mikeebowen@users.noreply.github.com>
2025-01-09 08:33:15 -0800

    clean up hot-to-add-custom-ui-to-a-spreadsheet

--- samples/spreadsheet/add_custom_ui/cs/Program.cs
+++ samples/spreadsheet/add_custom_ui/cs/Program.cs
@@ -1,35 +1,8 @@
-// <Snippet0>
 using DocumentFormat.OpenXml.Office.CustomUI;
 using DocumentFormat.OpenXml.Packaging;
-using System.IO;
-using System.Xml.Linq;
 
-static void AddCustomUI(string fileName, string customUIContent)
-{
-    // <Snippet1>
-    using (SpreadsheetDocument document = SpreadsheetDocument.Open(fileName, true))
-    // </Snippet1>
-    {
-        // <Snippet2>
-        // You can have only a single ribbon extensibility part.
-        // If the part doesn't exist, create it.
-        var part = document.RibbonExtensibilityPart;
-        if (part is null)
-        {
-            part = document.AddRibbonExtensibilityPart();
-        }
-        // </Snippet2>
-
-        // <Snippet3>
-        part.CustomUI = new CustomUI(customUIContent);
-        part.CustomUI.Save();
-        // </Snippet3>
-    }
-}
-// </Snippet0>
-
-string xml =
 // <Snippet4>
+string xml =
 @"<customUI xmlns=""http://schemas.microsoft.com/office/2006/01/customui"">
 	<ribbon>
 		<tabs>
@@ -46,3 +19,25 @@ string xml =
 
 // args[0] should be the absolute path to the AddCustomUI.xlsm created earlier in the tutorial.
 AddCustomUI(args[0], xml);
+
+// <Snippet0>
+static void AddCustomUI(string fileName, string customUIContent)
+{
+    // <Snippet1>
+    using (SpreadsheetDocument document = SpreadsheetDocument.Open(fileName, true))
+    // </Snippet1>
+    {
+        // <Snippet2>
+        // You can have only a single ribbon extensibility part.
+        // If the part doesn't exist, create it.
+        RibbonExtensibilityPart part = document.RibbonExtensibilityPart ?? document.AddRibbonExtensibilityPart();
+        // </Snippet2>
+
+        // <Snippet3>
+        part.CustomUI = new CustomUI(customUIContent);
+        part.CustomUI.Save();
+        // </Snippet3>
+    }
+}
+// </Snippet0>
+
--- samples/spreadsheet/add_custom_ui/vb/Program.vb
+++ samples/spreadsheet/add_custom_ui/vb/Program.vb
@@ -1,50 +1,44 @@
-' <Snippet0>
 Imports DocumentFormat.OpenXml.Office.CustomUI
 Imports DocumentFormat.OpenXml.Packaging
 
-Module MyModule
-
+Module Program
     Sub Main(args As String())
         ' <Snippet4>
         Dim xml As String =
-"<customUI xmlns=""http://schemas.microsoft.com/office/2006/01/customui"">
-	<ribbon>
-		<tabs>
-			<tab idMso=""TabAddIns"">
-				<group id=""Group1"" label=""Group1"">
-					<button id=""Button1"" label=""Click Me!"" showImage=""false"" onAction=""SampleMacro""/>
-				</group>
-			</tab>
-		</tabs>
-	</ribbon>
-</customUI>"
+        "<customUI xmlns=""http://schemas.microsoft.com/office/2006/01/customui"">
+	        <ribbon>
+		        <tabs>
+			        <tab idMso=""TabAddIns"">
+				        <group id=""Group1"" label=""Group1"">
+					        <button id=""Button1"" label=""Click Me!"" showImage=""false"" onAction=""SampleMacro""/>
+				        </group>
+			        </tab>
+		        </tabs>
+	        </ribbon>
+        </customUI>"
         ' </Snippet4>
-        ' args(0) contains the path to the AddCustomUI.xlsm file earlier in the tutorial.
-        XLAddCustomUI(args(0), xml)
+        '
+
+        ' args(0) should be the absolute path to the AddCustomUI.xlsm created earlier in the tutorial.
+        AddCustomUI(args(0), xml)
     End Sub
 
-
-    Public Sub XLAddCustomUI(ByVal fileName As String, ByVal customUIContent As String)
-
+    ' <Snippet0>
+    Sub AddCustomUI(fileName As String, customUIContent As String)
         ' <Snippet1>
         Using document As SpreadsheetDocument = SpreadsheetDocument.Open(fileName, True)
             ' </Snippet1>
-
             ' <Snippet2>
             ' You can have only a single ribbon extensibility part.
-            ' If the part doesn't exist, add it.
-            Dim part = document.RibbonExtensibilityPart
-            If part Is Nothing Then
-                part = document.AddRibbonExtensibilityPart
-            End If
+            ' If the part doesn't exist, create it.
+            Dim part = If(document.RibbonExtensibilityPart, document.AddRibbonExtensibilityPart())
             ' </Snippet2>
 
             ' <Snippet3>
             part.CustomUI = New CustomUI(customUIContent)
             part.CustomUI.Save()
             ' </Snippet3>
-
         End Using
     End Sub
+    ' </Snippet0>
 End Module
-' </Snippet0>
