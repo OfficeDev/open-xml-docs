014cdecd338eebf32b8e43b641383661bf5d1825
Michael Bowen <10384982+mikeebowen@users.noreply.github.com>
2025-01-10 15:15:02 -0800

    clean up docs\spreadsheet\how-to-retrieve-a-list-of-the-hidden-rows-or-columns-in-a-spreadsheet.md

--- samples/spreadsheet/retrieve_a_list_of_the_hidden_rows_or_columns/cs/Program.cs
+++ samples/spreadsheet/retrieve_a_list_of_the_hidden_rows_or_columns/cs/Program.cs
@@ -1,10 +1,31 @@
-// <Snippet0>
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
+List<uint>? items = null;
+
+if (args is [{ } fileName, { } sheetName, { } detectRows])
+{
+    items = GetHiddenRowsOrCols(fileName, sheetName, detectRows);
+}
+else if (args is [{ } fileName2, { } sheetName2])
+{
+    items = GetHiddenRowsOrCols(fileName2, sheetName2);
+}
+
+if (items is null)
+{
+    throw new ArgumentException("Invalid arguments.");
+}
+
+foreach (uint item in items)
+{
+    Console.WriteLine(item);
+}
+
+// <Snippet0>
 static List<uint> GetHiddenRowsOrCols(string fileName, string sheetName, string detectRows = "false")
 {
     // Given a workbook and a worksheet name, return 
@@ -12,9 +33,9 @@ static List<uint> GetHiddenRowsOrCols(string fileName, string sheetName, string 
     // of hidden column numbers. If detectRows is true, return
     // hidden rows. If detectRows is false, return hidden columns. 
     // Rows and columns are numbered starting with 1.
-    // <Snippet1>
     List<uint> itemList = new List<uint>();
 
+    // <Snippet1>
     using (SpreadsheetDocument document = SpreadsheetDocument.Open(fileName, false))
     {
         if (document is not null)
@@ -24,16 +45,16 @@ static List<uint> GetHiddenRowsOrCols(string fileName, string sheetName, string 
 
             // <Snippet2>
             Sheet? theSheet = wbPart.Workbook.Descendants<Sheet>().FirstOrDefault((s) => s.Name == sheetName);
+            // </Snippet2>
 
             if (theSheet is null || theSheet.Id is null)
             {
                 throw new ArgumentException("sheetName");
             }
-            // </Snippet2>
             else
             {
-                // <Snippet3>
 
+                // <Snippet3>
                 // The sheet does exist.
                 WorksheetPart? wsPart = wbPart.GetPartById(theSheet.Id!) as WorksheetPart;
                 Worksheet? ws = wsPart?.Worksheet;
@@ -78,24 +99,3 @@ static List<uint> GetHiddenRowsOrCols(string fileName, string sheetName, string 
     return itemList;
 }
 // </Snippet0>
-
-List<uint>? items = null;
-
-if (args is [{ } fileName, { } sheetName, { } detectRows])
-{
-    items = GetHiddenRowsOrCols(fileName, sheetName, detectRows);
-}
-else if (args is [{ } fileName2, { } sheetName2])
-{
-    items = GetHiddenRowsOrCols(fileName2, sheetName2);
-}
-
-if (items is null)
-{
-    throw new ArgumentException("Invalid arguments.");
-}
-
-foreach (uint item in items)
-{
-    Console.WriteLine(item);
-}
--- samples/spreadsheet/retrieve_a_list_of_the_hidden_rows_or_columns/vb/Program.vb
+++ samples/spreadsheet/retrieve_a_list_of_the_hidden_rows_or_columns/vb/Program.vb
@@ -1,72 +1,81 @@
-' <Snippet0>
 Imports DocumentFormat.OpenXml.Packaging
 Imports DocumentFormat.OpenXml.Spreadsheet
 
 Module Program
     Sub Main(args As String())
+        Dim items As List(Of UInteger) = Nothing
+
+        If args.Length = 3 Then
+            items = GetHiddenRowsOrCols(args(0), args(1), args(2))
+        ElseIf args.Length = 2 Then
+            items = GetHiddenRowsOrCols(args(0), args(1))
+        Else
+            Throw New ArgumentException("Invalid arguments.")
+        End If
+
+        For Each item As UInteger In items
+            Console.WriteLine(item)
+        Next
     End Sub
 
-
-    Public Function GetHiddenRowsOrCols(
-      ByVal fileName As String, ByVal sheetName As String,
-      ByVal detectRows As Boolean) As List(Of UInteger)
-
-        ' Given a workbook and a worksheet name, return either 
-        ' a list of hidden row numbers, or a list of hidden 
-        ' column numbers. If detectRows is True, return
-        ' hidden rows. If detectRows is False, return hidden columns. 
+    ' <Snippet0>
+    Function GetHiddenRowsOrCols(fileName As String, sheetName As String, Optional detectRows As String = "false") As List(Of UInteger)
+        ' Given a workbook and a worksheet name, return 
+        ' either a list of hidden row numbers, or a list 
+        ' of hidden column numbers. If detectRows is true, return
+        ' hidden rows. If detectRows is false, return hidden columns. 
         ' Rows and columns are numbered starting with 1.
+        Dim itemList As New List(Of UInteger)()
 
         ' <Snippet1>
-        Dim itemList As New List(Of UInteger)
+        Using document As SpreadsheetDocument = SpreadsheetDocument.Open(fileName, False)
+            If document IsNot Nothing Then
+                Dim wbPart As WorkbookPart = If(document.WorkbookPart, document.AddWorkbookPart())
+                ' </Snippet1>
 
-        Using document As SpreadsheetDocument =
-            SpreadsheetDocument.Open(fileName, False)
-
-            Dim wbPart As WorkbookPart = document.WorkbookPart
-            ' </Snippet1>
-
-            ' <Snippet2>
-            Dim theSheet As Sheet = wbPart.Workbook.Descendants(Of Sheet)().
-                Where(Function(s) s.Name = sheetName).FirstOrDefault()
-            If theSheet Is Nothing Then
-                Throw New ArgumentException("sheetName")
+                ' <Snippet2>
+                Dim theSheet As Sheet = wbPart.Workbook.Descendants(Of Sheet)().FirstOrDefault(Function(s) s.Name = sheetName)
                 ' </Snippet2>
 
-                ' <Snippet3>
-            Else
-                ' The sheet does exist.
-                Dim wsPart As WorksheetPart = CType(wbPart.GetPartById(theSheet.Id), WorksheetPart)
-                Dim ws As Worksheet = wsPart.Worksheet
-                ' </Snippet3>
-
-                If detectRows Then
-                    ' Retrieve hidden rows.
-                    ' <Snippet4>
-                    itemList = ws.Descendants(Of Row).
-                        Where(Function(r) r.Hidden IsNot Nothing AndAlso
-                              r.Hidden.Value).
-                        Select(Function(r) r.RowIndex.Value).ToList()
-                    ' </Snippet4>
+                If theSheet Is Nothing OrElse theSheet.Id Is Nothing Then
+                    Throw New ArgumentException("sheetName")
                 Else
-                    ' Retrieve hidden columns.
-                    ' <Snippet5>
-                    Dim cols = ws.Descendants(Of Column).
-                        Where(Function(c) c.Hidden IsNot Nothing AndAlso
-                              c.Hidden.Value)
-                    For Each item As Column In cols
-                        For i As UInteger = item.Min.Value To item.Max.Value
-                            itemList.Add(i)
-                        Next
-                    Next
-                    ' </Snippet5>
+                    ' <Snippet3>
+                    ' The sheet does exist.
+                    Dim wsPart As WorksheetPart = TryCast(wbPart.GetPartById(theSheet.Id), WorksheetPart)
+                    Dim ws As Worksheet = wsPart?.Worksheet
+                    ' </Snippet3>
 
+                    If ws IsNot Nothing Then
+                        If detectRows.ToLower() = "true" Then
+                            ' <Snippet4>
+                            ' Retrieve hidden rows.
+                            itemList = ws.Descendants(Of Row)() _
+                                .Where(Function(r) r?.Hidden IsNot Nothing AndAlso r.Hidden.Value) _
+                                .Select(Function(r) r.RowIndex?.Value) _
+                                .Cast(Of UInteger)() _
+                                .ToList()
+                            ' </Snippet4>
+                        Else
+                            ' Retrieve hidden columns.
+                            ' <Snippet5>
+                            Dim cols = ws.Descendants(Of Column)().Where(Function(c) c?.Hidden IsNot Nothing AndAlso c.Hidden.Value)
+
+                            For Each item As Column In cols
+                                If item.Min IsNot Nothing AndAlso item.Max IsNot Nothing Then
+                                    For i As UInteger = item.Min.Value To item.Max.Value
+                                        itemList.Add(i)
+                                    Next
+                                End If
+                            Next
+                            ' </Snippet5>
+                        End If
+                    End If
                 End If
-
             End If
         End Using
+
         Return itemList
     End Function
+    ' </Snippet0>
 End Module
-
-' </Snippet0>
